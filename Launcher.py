f7ed4c5f06b97dff73e6fce1e7bb5a432c69e4c5
Rohan Gupta <guptarohan323@gmail.com>
2020-01-29 13:04:56 -0600

    Finished Launcher.py, username and password can be generated

--- Launcher.py
+++ Launcher.py
@@ -5,7 +5,7 @@ import random
 #inputs: length=the length of the username, hasLetters= boolean value to indicate if a username has letters, hasNumbers=boolean value 
 #to indicate if it has numbers, hasSpecialChar=boolean value that indicates if there is any special characters
 def genUsername(length, hasLetters, hasNumbers, hasSpecialChar): 
-    return generate(length, hasLetters, hasNumbers, hasSpeicalChar); 
+    return generate(length, hasLetters, hasNumbers, hasSpecialChar); 
 
 def generate(length, hasLetters, hasNumbers, hasSpecialChar): 
     letters="abcdefghijklmnopqrstuvwxyz"+"abcdefghijklmnopqrstuvwxyz".upper(); 
